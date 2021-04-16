7edfc91bd7611c75416ccf419d4ee48a8e2fdd7f
Sagar kumar <68368273+Rajputsagar@users.noreply.github.com>
2021-04-16 21:05:55 +0530

    Add files via upload

---
+++ 2 variable and datatype programme.py
@@ -0,0 +1,16 @@
+#this programme is for to understand how the use of variables and datatype
+x = '''sagar'''
+y = 121
+z = 12.6
+a = None
+b = True
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
