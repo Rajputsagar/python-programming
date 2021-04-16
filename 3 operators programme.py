cbc968a24052ee6a2ff686e42bbff3d1bfb71b9c
Sagar kumar <68368273+Rajputsagar@users.noreply.github.com>
2021-04-16 21:06:18 +0530

    Add files via upload

---
+++ 3 operators programme.py
@@ -0,0 +1,34 @@
+a = 10
+b = 8
+#airthmetic operator
+print("the value of 10+8 is",10+8)
+print("the value of 10-8 is",10-8)
+print("the value of 10*8 is",10*8)
+print("the value of 10/8 is",10/8)
+
+#assignment operators
+a = 40
+a += 10
+a -= 10
+a *= 10
+a /= 10
+print(a)
+
+#comprasion operators
+b = (10>5)
+print(b)
+c = (10<5)
+print(c)
+d = (10<=5)
+print(d)
+e = (10==5)
+print(e)
+f = (10!=5)
+print(f)
+
+#logical operators
+a = True
+b = False
+print("the value of a and b is",(a and b))
+print("the value of a or b is",(a or b))
+print("the value of not b is",(not b))
