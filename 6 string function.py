46a071876e26d69d8e999d97285014c84d52e682
Sagar kumar <68368273+Rajputsagar@users.noreply.github.com>
2021-04-17 22:32:31 +0530

    Add files via upload

---
+++ 6 string function.py
@@ -0,0 +1,21 @@
+story = "once upon a time there was a student named sagar rajpoot who uploaded all python programme of his course on github account"
+
+# string function
+
+# [len() function] (it count the length of a string)
+print(len(story))
+
+# [string.endswith("string which is by end")](it tell string completed by this string or not)
+print(story.endswith("account"))
+
+# [string.count("string name which is to be counted")]
+print(story.count("o"))
+
+# [string.capitalize()](this function used to capital the first letter of the string)
+print(story.capitalize())
+
+# [string.find("word")](it find the word which is to be find)
+print(story.find("sagar"))
+
+# [string.replace("oldword","newword")](it replacethe old word to new word)
+print(story.replace("rajpoot","rajput"))
