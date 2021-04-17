f1508f62b709dd0b0d60261b1437c4d0f0cbb1c2
Sagar kumar <68368273+Rajputsagar@users.noreply.github.com>
2021-04-17 22:32:49 +0530

    Add files via upload

---
+++ 7 list method.py
@@ -0,0 +1,26 @@
+# method of list
+l1 = [1,6,3,4,9,23,19]
+
+# list sort
+l1.sort()
+print(l1)
+
+# list rverse
+l1.reverse()
+print(l1)
+
+# appends the list
+l1.append(35) #add 35 at the end of list
+print(l1)
+
+# insert the no. in the list
+l1.insert(3, 8)  #insert the 8 at the index 0f 3
+print(l1)
+
+# pop in list
+l1.pop(3)  # it will delete the value at the given index
+print(l1)  
+
+# remove the element from the list
+l1.remove(23)
+print(l1)
